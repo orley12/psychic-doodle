5265af6a8ac8bb68c6b66f7fe41e33356ee0b12e
orley12 <oosolarin@gmail.com>
2024-07-05 23:59:17 +0100

    Initial Commit

---
+++ RickMorty/RickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApp.swift
+//  RickMorty
+//
+//  Created by Oluwatosin Solarin on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
